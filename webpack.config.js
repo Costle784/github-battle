b3265c731f004b303735b41948a4e12e236aada2
Curtis Ostle <curtisostle@gmail.com>
2017-09-20 09:32:00 -0400

    reconfigured webpack and package.json to build for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
-  entry:'./app/index.js',
+var config = {
+  entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
@@ -15,9 +16,24 @@ module.exports = {
     ]
   },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'app/index.html'
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
 }
+
+module.exports = config;
